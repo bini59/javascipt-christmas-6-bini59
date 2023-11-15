3ced864a150f41120546c023aa77d1b90a2a1d24
bini59 <myuni0234@gmail.com>
2023-11-15 20:26:05 +0900

    feat(input): 날짜, 메뉴 입력 method 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import InputView from './InputView.js';
+
 class App {
-  async run() {}
+
+  async inputDate() {
+    const date = await InputView.readDate();
+    return date;
+  }
+
+  async inputMenus() {
+    const menus = await InputView.readMenus();
+    return menus;
+  }
+
+  async run() {
+    const date = await this.inputDate();
+    const menus = await this.inputMenus();
+    console.log(date);
+    console.log(menus);
+  }
 }
 
 export default App;
