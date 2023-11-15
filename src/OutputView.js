ed4bffd1c4a4ce5d8b8c7b47651e281f6d490523
bini59 <myuni0234@gmail.com>
2023-11-15 22:39:26 +0900

    style(tab): 탭 사이즈를 2로 조정

--- src/OutputView.js
+++ src/OutputView.js
@@ -2,56 +2,56 @@ import { Console } from "@woowacourse/mission-utils";
 import utils from './modules/utils.js';
 
 const OutputView = {
-    printMenu(menus) {
-        Console.print("<주문 메뉴>");
-        for (let i = 0; i < menus.length; i += 1) {
-            Console.print(`${menus[i][0]} ${menus[i][1]}개`);
-        }
-    },
-    
-    printTotalPrice(totalPrice) {
-        Console.print('<할인 전 총주문 금액>');
-        Console.print(`${utils.getMoneyString(totalPrice)}원`);
-    },
-
-    printGiveaway(giveaway) {
-        Console.print('<증정 메뉴>');
-        if (giveaway === undefined) {
-            Console.print('없음');
-            return;
-        }
-        Console.print(`${giveaway[0]} ${giveaway[1]}개`);
-    },
-
-    printBenefit(benefits) {
-        Console.print('<혜택 내역>');
-        if (benefits.length === 0) {
-            Console.print('없음');
-            return;
-        }
-        for (let i = 0; i < benefits.length; i += 1) {
-            Console.print(`${benefits[i][0]}: -${utils.getMoneyString(benefits[i][1])}원`);
-        }
-    },
-
-    printBenefitPrice(price) {
-        Console.print('<총혜택 금액>');
-        Console.print(`${utils.getMoneyString(price)}원`);
-    },
-
-    printFinalPrice(finalPrice) {
-        Console.print('<할인 후 예상 결제 금액>');
-        Console.print(`${utils.getMoneyString(finalPrice)}원`);
-    },
-
-    printBadge(badge) {
-        Console.print('<12월 이벤트 배지>');
-        if (badge === undefined) {
-            Console.print('없음');
-            return;
-        }
-        Console.print(badge);
+  printMenu(menus) {
+    Console.print("<주문 메뉴>");
+    for (let i = 0; i < menus.length; i += 1) {
+      Console.print(`${menus[i][0]} ${menus[i][1]}개`);
     }
+  },
+  
+  printTotalPrice(totalPrice) {
+    Console.print('<할인 전 총주문 금액>');
+    Console.print(`${utils.getMoneyString(totalPrice)}원`);
+  },
+
+  printGiveaway(giveaway) {
+    Console.print('<증정 메뉴>');
+    if (giveaway === undefined) {
+      Console.print('없음');
+      return;
+    }
+    Console.print(`${giveaway[0]} ${giveaway[1]}개`);
+  },
+
+  printBenefit(benefits) {
+    Console.print('<혜택 내역>');
+    if (benefits.length === 0) {
+      Console.print('없음');
+      return;
+    }
+    for (let i = 0; i < benefits.length; i += 1) {
+      Console.print(`${benefits[i][0]}: -${utils.getMoneyString(benefits[i][1])}원`);
+    }
+  },
+
+  printBenefitPrice(price) {
+    Console.print('<총혜택 금액>');
+    Console.print(`${utils.getMoneyString(price)}원`);
+  },
+
+  printFinalPrice(finalPrice) {
+    Console.print('<할인 후 예상 결제 금액>');
+    Console.print(`${utils.getMoneyString(finalPrice)}원`);
+  },
+
+  printBadge(badge) {
+    Console.print('<12월 이벤트 배지>');
+    if (badge === undefined) {
+      Console.print('없음');
+      return;
+    }
+    Console.print(badge);
+  }
 }
 
 export default OutputView;
